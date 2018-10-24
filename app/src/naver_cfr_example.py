07edc84240b8d4aefadbcba0fbb5730c9e491fb5
sykim <sykim.lecture@gmail.com>
2018-10-24 11:07:02 +0900

    Update naver_cfr_example.py
    주석추가

--- app/src/naver_cfr_example.py
+++ app/src/naver_cfr_example.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import requests  # 인식 안될경우 : pip install requests 
-client_id = "CLIENT_ID"
-client_secret = "CLIENT_SECRET"
+client_id = "CLIENT_ID"  # 발급받은 id로 변경
+client_secret = "CLIENT_SECRET" # 발급받은 secret로 변경
 # url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
 url = "https://openapi.naver.com/v1/vision/celebrity" # 유명인 얼굴인식
 files = {'image': open('D:/workspace/workspace_python/......./sample.jpg', 'rb')}
