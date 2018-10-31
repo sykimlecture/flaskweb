e079661f334a30a5edf9dd9e387eb1afd6cf5a17
sykim <sykim.lecture@gmail.com>
2018-10-31 17:52:11 +0900

    Update keras_test.py
    케라스 구동 테스트

--- app/src/keras_test.py
+++ app/src/keras_test.py
@@ -1,9 +1,25 @@
 import tensorflow
 import keras
 from keras.models import Sequential
+from keras.utils import np_utils
+from keras.datasets import mnist
+from keras.layers import Dense, Activation
 
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+X_train = X_train.reshape(60000, 784).astype('float32') / 255.0
+X_test = X_test.reshape(10000, 784).astype('float32') / 255.0
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
 
 model = Sequential()
+model.add(Dense(units=64, input_dim=28*28, activation='relu'))
+model.add(Dense(units=10, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.fit(X_train, Y_train, epochs=5, batch_size=32)
 
-print('tensorflow ' + tensorflow.__version__)
-print('keras ' + keras.__version__)
+loss_and_metrics = model.evaluate(X_test, Y_test, batch_size=32)
+
+print('loss_and_metrics : ' + str(loss_and_metrics))
+
+# print('tensorflow ' + tensorflow.__version__)
+# print('keras ' + keras.__version__)
